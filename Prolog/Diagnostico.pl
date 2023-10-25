7191c3f283d1d51b2be1f058ac879942c1709826
Etec <etec@etec.com>
2023-10-25 20:14:00 -0300

    [UPDATE] Diagnostico.pl

---
+++ Prolog/Diagnostico.pl
@@ -0,0 +1,11 @@
+%% Fatos
+sexo(feminino).
+idade(12).
+possui_doenca_cardiaca(sim).
+possui_doenca_respiratoria(sim).
+possui_doenca_himunologica(sim).
+historico_doenca_cardiaca(sim).
+historico_doenca_respiratoria(sim).
+historico_doenca_himunologica(sim).
+
+%% Regas
