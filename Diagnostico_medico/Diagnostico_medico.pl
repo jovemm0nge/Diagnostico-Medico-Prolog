df8c850e2c15b6c8418801c786c712b426c20df8
Gabriel Arthur Tonholo de Campos <gabrielarthurtonholodecampos@gmail.com>
2023-10-27 16:56:15 -0300

    [Update] Diagnostico_medico.pl Fatos

--- Diagnostico_medico/Diagnostico_medico.pl
+++ Diagnostico_medico/Diagnostico_medico.pl
@@ -1,11 +1,49 @@
-%% Fatos
-sexo(feminino).
-idade(12).
-possui_doenca_cardiaca(sim).
-possui_doenca_respiratoria(sim).
-possui_doenca_himunologica(sim).
-historico_doenca_cardiaca(sim).
-historico_doenca_respiratoria(sim).
-historico_doenca_himunologica(sim).
+%% -> Fatos
 
-%% Regas
+%% Maria
+sexo(maria,feminino).
+idade(maria,59).
+problema_cardiaco(maria, sim).
+problema_respiratoria(maria, sim).
+problema_himunologica(maria, sim).
+problema_cardiaca(maria, sim).
+problema_respiratoria(maria, sim).
+problema_himunologica(maria, sim).
+historico_cardiaco(maria,sim).
+historico_respiratoria(maria,sim).
+historico_himunologica(maria,sim).
+historico_cardiaca(maria,sim).
+historico_respiratoria(maria,sim).
+historico_himunologica(maria,sim).
+
+sintoma_A(maria, sim)
+sintoma_B(maria, sim)
+sintoma_C(maria, sim)
+sintoma_D(maria, sim)
+sintoma_E(maria, sim)
+sintoma_F(maria, sim)
+
+%% João
+sexo(joao,masculino).
+idade(joao,12).
+problema_cardiaco(joao, nao).
+problema_respiratoria(joao, nao).
+problema_himunologica(joao, nao).
+problema_cardiaca(joao, nao).
+problema_respiratoria(joao, nao).
+problema_himunologica(joao, nao).
+historico_cardiaco(joao,nao).
+historico_respiratoria(joao,nao).
+historico_himunologica(joao,nao).
+historico_cardiaca(joao,nao).
+historico_respiratoria(joao,nao).
+historico_himunologica(joao,nao).
+
+sintoma_A(joao, sim)
+sintoma_B(joao, sim)
+sintoma_C(joao, sim)
+sintoma_D(joao, sim)
+sintoma_E(joao, sim)
+sintoma_F(joao, sim)
+
+%% -> Regras
