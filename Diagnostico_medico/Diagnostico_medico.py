b1688f281e6523e5364fcf769022caa855ce7f83
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-11-01 19:04:30 -0300

    Update Diagnostico_medico.py

--- Diagnostico_medico/Diagnostico_medico.py
+++ Diagnostico_medico/Diagnostico_medico.py
@@ -1,5 +1,36 @@
 from pyswip import Prolog
-
 prolog = Prolog()
+prolog.consult("diagnostico_medico2.pl")
 
-prolog.consult("Diagnostico_medico.pl")
+# Defina os sintomas inseridos pelo paciente
+sintomas_do_paciente = ["febre", "tosse"]
+
+# Crie uma consulta Prolog com variáveis
+consulta = "doenca(D, S), member(S, ["
+consulta += ", ".join(f"'{s}'" for s in sintomas_do_paciente)
+consulta += "])."
+
+# Execute a consulta
+condicoes_possiveis = list(prolog.query(consulta))
+
+# Imprima os resultados
+if condicoes_possiveis:
+    print("Condições médicas possíveis:")
+    for soln in condicoes_possiveis:
+        print(soln["D"])
+else:
+    print("Não foi possível fazer um diagnóstico com os sintomas fornecidos.")
+
+
+# print(list(prolog.query("sintoma(gripe, X)")) == [{'X': 'febre'}, {'X': 'tosse'}, {'X': 'dor_de_garganta'}])
+#
+# # for soln in prolog.query("sintoma(X,Y)"):
+# #     print(soln["X"], "é sintoma de", soln["Y"])
+#
+# sintomas = []
+#
+# for soln in prolog.query("sintoma(gripe,X)"):
+#     print(soln["X"], "é sintoma de gripe")
+#     sintomas.append(soln["X"])
+#
+# print(sintomas)
