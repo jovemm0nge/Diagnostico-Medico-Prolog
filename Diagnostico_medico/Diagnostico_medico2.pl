a4ca3d8b2975c034a4481f25d81beb5f3de75e4a
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-11-16 19:24:36 -0300

    arquivos redundantes removidos

--- Diagnostico_medico/Diagnostico_medico2.pl
+++
@@ -1,9 +0,0 @@
-%% -> Fatos
-
-sintoma(gripe, febre).
-sintoma(gripe, tosse).
-sintoma(gripe, dor_de_garganta).
-sintoma(resfriado, tosse).
-sintoma(resfriado, nariz_entupido).
-
-doenca(D, S) :- sintoma(D, S).
